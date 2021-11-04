0a95bb6f14ef4009d39fb29c54b7639f03981aab
jointri <81046576+jointri@users.noreply.github.com>
2021-11-04 09:39:36 -0400

    Add files via upload

---
+++ playaudiokrita.sh
@@ -0,0 +1,63 @@
+#THIS SCRIPT IS LICENSED UNDER CC0, IN OTHER WORDS, PUBLIC DOMAIN.
+#YOU CAN DO WHATEVER YOU WANT WITH IT. I DON'T CARE, IT'S INCREDIBLY TRIVIAL AND MINIMALIST (AND NOT IMPRESSIVE)
+#THAT IT DOESN'T JUSTIFY USING GPL.
+
+
+#!/bin/sh
+
+echo "The audio file will begin in 10 seconds. Press Ctrl + c to terminate the script."
+
+sleep 2
+
+
+echo "BE SURE TO PRESS PLAY ON THE TIMELINE WHEN THE COUNTDOWN HITS 0 AND YOU HEAR SOMETHING PLAYING!"
+
+sleep 2
+
+echo "BEGINNING... NOW!"
+
+echo 10
+
+sleep 1
+
+echo 9
+
+sleep 1
+
+echo 8
+
+sleep 1
+
+echo 7
+
+sleep 1
+
+echo 6
+
+sleep 1
+
+echo 5
+
+sleep 1
+
+echo 4
+
+sleep 1
+
+echo 3
+
+sleep 1
+
+echo 2
+
+sleep 1
+
+echo 1
+
+sleep 1
+
+echo 0!
+
+#INSERT COMMAND HERE TO PLAY THE AUDIO FILE IN THE MUSIC PLAYER OF YOUR CHOICE.
+
+
